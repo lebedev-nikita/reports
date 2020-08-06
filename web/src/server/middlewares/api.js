3409efb53b8c01541d8057890f9f898d60063815
Nikita Lebedev <y87cgp@gmail.com>
2020-08-06 19:23:22 +0300

    added /api/ for api urls #1

--- web/src/server/middlewares/api.js
+++ web/src/server/middlewares/api.js
@@ -8,27 +8,18 @@ const { invite } = require('./requests/invite');
 const { uninvite } = require('./requests/uninvite');
 
 
-const { client } = require('../services/pg');
-
 module.exports = function (app) {
     // -
-    app.post('/start', start);
+    app.post('/api/start', start);
     // ++
-    app.get('/reports', reports);
+    app.get('/api/reports', reports);
     // ++
-    app.get('/progress', execution);
+    app.get('/api/progress', execution);
     // +?
-    app.get('/download', writeResult);
+    app.get('/api/download', writeResult);
     // ++
-    app.get('/history', history);
+    app.get('/api/history', history);
 
-    app.get('/invite', invite);
-    app.get('/uninvite', uninvite);
-
-    app.get('/api/getTest2', async (req, res) => {
-
-        const { rows: data } = await client.query('SELECT * FROM userdata.reports_execution LIMIT 100');
-        res.send(data);
-    });
-
+    app.get('/api/invite', invite);
+    app.get('/api/uninvite', uninvite);
 };
